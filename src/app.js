ce8fc22cc72581ef2aec6adeeb61d71c270e5cc4
andfaulkner <andfaulkner@gmail.com>
2015-08-12 18:32:20 -0400

    cleaned app.js route experiments out; single prototype route object-based routing now working

--- src/app.js
+++ src/app.js
@@ -30,35 +30,11 @@
 
     let routes = require('../config/routes.json'); //Get route list
 
-    console.log("just before iife-tpl route registration");
-
     //uses route constructor on all routes registered in the config object
     routes.topLevelRoutes.forEach(function(routeOpts){
         let file = routeOpts.file || routeOpts.route;
         app.use('/' + routeOpts.route, require('routes/proto_route.js')(file));
     });
-
-    // //uses route constructor
-    // app.use('/' + routes.testRoute.route,
-    //     require(routes.routeDir + '/' +
-    //             'proto_route.js')(routes.testRoute.route));
-    //              // routes.testRoute.route + routes.routeSuffix)(routes.testRoute.route));
-
-    // uses route module that takes a parameter
-    // app.use('/' + routes.testRoute.route,
-    //     require(routes.routeDir + '/' +
-    //              routes.testRoute.route + routes.routeSuffix)(routes.testRoute.route));
-
-    console.log("passes iife-tpl route registration");
-
-    // //Create routes from route list items
-    // routes.topLevelRoutes.forEach(function(rObj){
-    //     rObj.file = rObj.file || rObj.route;
-
-    //     //load an individual route-compiling module (e.g. routes/login_route.js)
-    //     let routeFile = routes.routeDir + '/' + rObj.file + routes.routeSuffix;
-    //     app.use('/' + rObj.route, (require(routeFile)));
-    // });
   /********************************************************************/
 
     // ERROR HANDLING
